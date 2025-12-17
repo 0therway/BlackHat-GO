2148f94ec3ed1055585267100d1ab24864924416
Otherway~ <lopeznico@scas.ph.education>
2025-12-17 12:00:52 +0800

    Add files via upload
    Go program about different variables and the use :=

---
+++ var1.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var title string = "Simple man dont now how to code"
+	subtitle := "but know how to destroy things"
+	var month int = 12
+	date := 17
+	var year int = 2025
+
+	fmt.Println(title)
+	fmt.Println(subtitle)
+	fmt.Println(month, "/", date, "/", year)
+
+}
