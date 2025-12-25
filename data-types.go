dba90391e346166ee78d1cebf4a6ac56a89d5594
Otherway~ <lopeznico@scas.ph.education>
2025-12-25 12:08:19 +0800

    Add files via upload

---
+++ data-types.go
@@ -0,0 +1,18 @@
+package main 
+
+import ("fmt")
+//data types
+var(
+	w string = "Good Morning"
+	x int = 5555
+	y float32 = 6.7
+	z bool = (3>1)
+)
+func main (){
+	fmt.Println("\nThis are Data types\n")
+	fmt.Println("String: ", w)
+	fmt.Println("int: ", x)
+	fmt.Println("float32: ", y)
+	fmt.Println("boolean: ", z)
+
+}
